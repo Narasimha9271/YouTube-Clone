a99d4e1b941474df31f7a86ba32fc322c6420f02
Narasimha P L <107346073+Narasimha9271@users.noreply.github.com>
2023-03-24 17:29:10 +0530

    added-shimmer

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -2,30 +2,31 @@ import React, { useEffect, useState } from "react";
 import VideoCard from "./VideoCard";
 import { YOUTUBE_VIDEO_API } from "../utils/constants";
 import { Link } from "react-router-dom";
+import Shimmer from "./Shimmer";
 
 const VideoContainer = () => {
-  //We need a state variable to loop on this videos
-  const [videos, setvideos] = useState([]);
-  useEffect(() => {
-    getVideos();
-  }, []);
-  const getVideos = async () => {
-    const data = await fetch(YOUTUBE_VIDEO_API);
-    const json = await data.json();
+    //We need a state variable to loop on this videos
+    const [videos, setvideos] = useState([]);
+    useEffect(() => {
+        getVideos();
+    }, []);
+    const getVideos = async () => {
+        const data = await fetch(YOUTUBE_VIDEO_API);
+        const json = await data.json();
+        setvideos(json.items);
+    };
 
-    setvideos(json.items);
-  };
-
-  if (!videos) return null;
-  return (
-    <div className="flex flex-wrap ">
-      {videos.map((video) => (
-        <Link to={"/watch?v=" + video.id}>
-          <VideoCard key={video.id} info={video} />
-        </Link>
-      ))}
-    </div>
-  );
+    return videos?.length === 0 ? (
+        <Shimmer />
+    ) : (
+        <div className="flex flex-wrap ">
+            {videos.map((video) => (
+                <Link to={"/watch?v=" + video.id}>
+                    <VideoCard key={video.id} info={video} />{" "}
+                </Link>
+            ))}
+        </div>
+    );
 };
 
 export default VideoContainer;
